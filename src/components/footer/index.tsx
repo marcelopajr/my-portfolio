2c9f8740f54a827640fbe171e3ed91a0ed30b664
Marcelo Pereira <marcelo96jr@gmail.com>
2024-06-05 22:21:18 -0300

    chore: update linkedin url

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,7 +6,10 @@ export const Footer = () => {
     <footer className="h-14 w-full flex items-center justify-center bg-gray-950">
       <span className="flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400">
         Made with <IoMdHeart className="text-emerald-500" /> by
-        <Link href="https://linkedin.com/in/marcelopajr" target="_blank">
+        <Link
+          href="https://www.linkedin.com/in/marcelopajr/?locale=en_US"
+          target="_blank"
+        >
           <strong className="font-medium">Marcelo Almeida</strong>
         </Link>
       </span>
